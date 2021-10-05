7ce1471386ff53513a7b346d13f73fff7c91612a
Artem Lavr <lavart242@gmail.com>
2021-10-05 13:32:31 +0300

    subm commit

--- config.py
+++
@@ -1,6 +0,0 @@
-settings = {
-    'token': 'ODk0NjY2NzM1NTkwOTY5Mzc0.YVtVUw.0rBVi6t4v8jaF2zGeGevnUrwAlY',
-    'bot': 'Имя бота',
-    'id': Client ID бота, без кавычек,
-    'prefix': 'Префикс бота'
-}
